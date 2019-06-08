e2d4810cb2fe7c1afa5951329a0f0dab0c40f743
dw068 <dw068@hdm-stuttgart.de>
2019-06-08 13:28:59 +0200

    initial commit

---
+++ src/main/java/projectileGame/App.java
@@ -0,0 +1,20 @@
+package projectileGame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App extends Application
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+    }
+}
